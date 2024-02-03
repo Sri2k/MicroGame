d2da086c05ce226ccf4cebf60664245a069979d8
sri sai <gandusrisai@gmail.com>
2024-02-04 00:08:38 +0530

    repos added with mongodb crud operations

---
+++ src/Play.Catalog.Service/Extensions.cs
@@ -0,0 +1,11 @@
+using Play.Catalog.Services.Dtos;
+
+namespace Play.Catalog.Service
+{
+    public static class Extensions
+    {
+        public static ItemDto AsDto(this Item item){
+            return new ItemDto(item.Id, item.Name, item.Description, item.Price, item.CreatedDate);
+        }
+    }
+}
