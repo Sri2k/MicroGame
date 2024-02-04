241e226c8d3c4a2c823dd516919daca55f189ef2
sri sai <gandusrisai@gmail.com>
2024-02-04 12:24:35 +0530

    damn files

--- src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -68,7 +68,7 @@ namespace Play.Catalog.Services.Controllers
             {
                 return NotFound();
             }
-            await itemRepository.RemoveAsync(exisitingItem.Id)
+            await itemRepository.RemoveAsync(exisitingItem.Id);
             return NoContent();
         }
 
